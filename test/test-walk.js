a91f404fc7acbc776ac7baabb170edb9adf3796e
AJ ONeal <awesome@coolaj86.com>
2014-05-20 17:12:07 -0600

    forEachAsync ~2.2 -> foreachasync 3.x

--- test/test-walk.js
+++ test/test-walk.js
@@ -9,10 +9,19 @@
 
   walker = walk(dirname);
 
+  walker.on('directories', function (root, stats, next) {
+    stats.forEach(function (stat) {
+      console.log('[ds]', path.join(root, stat.name));
+    });
+    next();
+  });
+
+  /*
   walker.on('directory', function (root, stat, next) {
     console.log('[d]', path.join(root, stat.name));
     next();
   });
+  */
 
   walker.on('file', function (root, stat, next) {
     console.log('[f]', path.join(root, stat.name));
